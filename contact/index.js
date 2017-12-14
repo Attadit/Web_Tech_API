7ac819e2c389820f0d8540e5a8a2ccb2bef5b66a
unknown <57660121@go.buu.ac.th>
2017-12-14 18:53:34 +0700

    third

--- contact/index.js
+++ contact/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 let contactList = [
-    {id: 0, name: 'Ned Stark', email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
-    {id: 1, name: 'Theon Greyjoy', email: 'tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
+    {id: 0, name: 'attadit Stark', email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
+    {id: 1, name: 'kittipoom Greyjoy', email: 'tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
     {id: 2, name: 'Samwell Tarly', email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Loyal brother of the watch.'},
-    {id: 3, name: 'Jon Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
+    {id: 3, name: 'anurak Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
     {id: 4, name: 'Arya Stark', email: 'waterdancer@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Has a list of names.'},
     {id: 5, name: 'Jora Mormont', email: 'khaleesifan100@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Lost in the friend-zone.'},
     {id: 6, name: 'Tyrion Lannister', email: 'tyrion@lannister.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Currently drunk.'},
@@ -16,8 +16,20 @@ let contactList = [
     {id: 11, name: 'Petyr Baelish', email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not trust anyone.'},
   ];
 
-router.get('/contacts', (req, res) => {
-    res.json(contactList)
+
+router.get('/contacts', (req, res) =>{
+	if(req.query.name){
+		for(i=0;i<contactList.length;i++)
+		{
+			if(contactList[i].name == req.query.name)
+			{
+				res.json(contactList[i])
+			}
+		}
+	}
+	else{
+		res.json(contactList)
+	}
 })
 
 router.get('/contacts/:id', (req, res) => {
@@ -38,6 +50,13 @@ router.put('/contacts/:id', (req, res) => {
     contactList[id] = contact
     res.status(200).json()
 })
+
+router.delete('/contacts/:id', (req, res) => {
+    let id = req.params.id
+    contactList.splice(id, 1)
+    res.status(204).json()
+})
+
 
 
 module.exports = router
